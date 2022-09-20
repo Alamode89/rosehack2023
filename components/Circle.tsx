25c443e12da256ab600ba1d8c5d2ab0dd837ba4b
Alamode89 <andreamante89@gmail.com>
2022-09-19 20:12:13 -0700

    trigger precommit

--- components/Circle.tsx
+++ components/Circle.tsx
@@ -7,7 +7,7 @@ interface props {
 const Circle = ({ hidden }: props) => {
   return (
     <div
-      className={`rounded-full hidden h-5 w-5 bg-rosehack-secondary-white lg:block`}
+      className="rounded-full hidden h-5 w-5 bg-rosehack-secondary-white lg:block"
     ></div>
   );
 };

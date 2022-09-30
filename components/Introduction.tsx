c65f79589d781f231d70538da6b9a8b915e5ad21
Divyank Shah <divyank.shah.2016@gmail.com>
2022-09-29 17:36:12 -0700

    center about

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const Introduction = () => {
   return (
     <div className="bg-rosehack-secondary-purple w-11/12 rounded-3xl p-4 md:text-3xl text-xl font-poppins">
-      <p className="leading-loose py-4 px-3">
-        Rose Hack is UC Riverside’s 24-hour hackathon, one of the few
+      <p className="leading-loose py-4 px-3 text-center">
+        Rose Hack is UC Riverside&#39;s 24-hour hackathon, one of the few
         women-centric hackathons in California! Founded by the female leaders of
         the Association for Computing Machinery (ACM-W) and the Society of Women
-        Engineers (SWE), Rose Hack’s mission is to encourage individuals from
-        underrepresented groups in STEM to gain experience, create strong
+        Engineers (SWE), Rose Hack&#39;s mission is to encourage individuals
+        from underrepresented groups in STEM to gain experience, create strong
         networks, and build amazing projects through their shared passions in
         Science and Technology. Rose Hack welcomes both undergraduate and high
         school students for a lively weekend filled with innovation, discovery,

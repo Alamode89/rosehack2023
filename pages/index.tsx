7c2beab430b0bf0bdb4955891eba67a36c34d759
Divyank Shah <divyank.shah.2016@gmail.com>
2022-11-04 18:41:00 -0700

    add landing & about

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next";
+import About from "../components/About";
+import Landing from "../components/Landing";
 import Sponsors from "../components/Sponsors";
 import Team from "../components/Team";
 
 const Home: NextPage = () => {
   return (
     <div className="flex justify-center items-center flex-col">
+      <Landing />
+      <About />
       <Sponsors />
       <Team />
     </div>

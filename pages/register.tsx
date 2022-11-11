9ca47b34641dfde9291afa4dd4b811fc0aa10eb5
Divyank Shah <divyank.shah.2016@gmail.com>
2022-11-11 13:56:47 -0800

    fix styling

--- pages/register.tsx
+++ pages/register.tsx
@@ -66,63 +66,95 @@ const Register = () => {
   };
 
   return (
-    <div className="flex justify-center items-center flex-col bg-gradient-to-b from-register-top to-register-bottom">
-      <div className="font-pixel text-4xl p-0 m-0 text-white text-center">
-        Register for Rosehack 2023
-      </div>
-      <input
-        type="text"
-        name="first"
-        value={user.first}
-        onChange={handleInput}
-        placeholder="First Name"
-        className="rounded-xl p-3"
-      />
-      <input
-        type="text"
-        name="last"
-        value={user.last}
-        onChange={handleInput}
-        placeholder="Last Name"
-        className="rounded-xl p-3"
-      />
-      <input
-        type="text"
-        name="email"
-        value={user.email}
-        onChange={handleInput}
-        placeholder="Email Address"
-        className="rounded-xl p-3"
-      />
-      <input
-        type="text"
-        name="phone"
-        value={user.phone}
-        onChange={handleInput}
-        placeholder="Phone Number"
-        className="rounded-xl p-3"
-      />
+    <div className="flex justify-center items-center flex-col bg-gradient-to-b from-about-top to-about-bottom w-full">
+      <div className="border-4 mt-5 rounded-3xl border-white drop-shadow-blue items-center flex flex-col w-2/3">
+        <div className="font-pixel text-4xl p-0 text-white text-center w-1/2 m-4">
+          &lt;REGISTER&gt;
+        </div>
+        <Row className="w-10/12 flex justify-between items-between">
+          <Col md={5}>
+            <label className="font-pixel text-md text-white text-center w-full drop-shadow-bluesmall">
+              first name
+            </label>
+            <input
+              type="text"
+              name="first"
+              value={user.first}
+              onChange={handleInput}
+              placeholder="First Name"
+              className="text-white rounded-xl p-2 w-full bg-transparent border-4 border-white drop-shadow-bluesmall"
+            />
+          </Col>
+          <Col md={5}>
+            <label className="font-pixel text-md text-white text-center w-full drop-shadow-bluesmall">
+              last name
+            </label>
+            <input
+              type="text"
+              name="last"
+              value={user.last}
+              onChange={handleInput}
+              placeholder="Last Name"
+              className="text-white rounded-xl p-2 w-full bg-transparent border-4 border-white drop-shadow-bluesmall"
+            />
+          </Col>
+        </Row>
+        <div className="my-2 w-10/12 ">
+          <label className="drop-shadow-bluesmall text-left font-pixel text-md text-white w-full ml-4">
+            email
+          </label>
+          <input
+            type="text"
+            name="email"
+            value={user.email}
+            onChange={handleInput}
+            placeholder="Email Address"
+            className="text-white rounded-xl p-2 w-full bg-transparent border-4 border-white drop-shadow-bluesmall"
+          />
+        </div>
+        <div className="my-2 w-10/12 ">
+          <label className="drop-shadow-bluesmall text-left font-pixel text-md text-white w-full ml-4">
+            phone number
+          </label>
 
-      <Selector
-        options={schools}
-        user={user}
-        field="school"
-        handleInput={handleInput}
-      />
-
-      <Selector
-        options={grades}
-        user={user}
-        field="grade"
-        handleInput={handleInput}
-      />
+          <input
+            type="text"
+            name="phone"
+            value={user.phone}
+            onChange={handleInput}
+            placeholder="Phone Number"
+            className="text-white rounded-xl p-2 w-full bg-transparent border-4 border-white drop-shadow-bluesmall"
+          />
+        </div>
+        <div className="w-10/12 flex justify-start ">
+          <label className="my-2 drop-shadow-bluesmall text-left font-pixel text-md text-white w-full ml-4">
+            school
+          </label>
+        </div>
+        <Row className="  w-10/12">
+          <Col md={8} className="m-0">
+            <Selector
+              options={schools}
+              user={user}
+              field="school"
+              handleInput={handleInput}
+            />
+          </Col>
+          <Col md={4}>
+            <Selector
+              options={grades}
+              user={user}
+              field="grade"
+              handleInput={handleInput}
+            />
+          </Col>
+        </Row>
 
         <Selector
           options={shirts}
           user={user}
           field="shirt"
           handleInput={handleInput}
-          
         />
 
         <Selector
@@ -146,6 +178,7 @@ const Register = () => {
           handleInput={handleInput}
         />
         <button className="p-3 bg-white">Submit</button>
+      </div>
     </div>
   );
 };

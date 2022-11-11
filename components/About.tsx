910db23070e8684e45bc1cc70cbe9e4b0d27156c
menthy-wu <wumengxuan0728@gmail.com>
2022-11-10 18:55:07 -0800

    format

--- components/About.tsx
+++ components/About.tsx
@@ -69,7 +69,10 @@ const About = () => {
           </div>
         </Col>
         <Col xl={4} md={6} className="flex items-center justify-center">
-          <img className="w-1/3 md:w-full pt-5 md:pt-0" src="MACHINE_3.png"></img>
+          <img
+            className="w-1/3 md:w-full pt-5 md:pt-0"
+            src="MACHINE_3.png"
+          ></img>
         </Col>
       </Row>
       <Row className="mt-5 sm:w-9/12 w-10/12 flex justify-center items-center flex-col-reverse md:flex-row-reverse">

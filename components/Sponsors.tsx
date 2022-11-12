f57d19ffe2a096bc6a9721039b13d4279ce79326
menthy-wu <wumengxuan0728@gmail.com>
2022-11-11 17:29:37 -0800

    format

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -6,37 +6,37 @@ const sponsors = [
   {
     pic: "triad.jpeg",
     link: "https://www.triadmagnetics.com/",
-    color:"#F5BB2C"
+    color: "#F5BB2C",
   },
   {
     pic: "desmos.png",
     link: "https://www.desmos.com",
-    color:"#148B47"
+    color: "#148B47",
   },
   {
     pic: "balsamiq.jpg",
     link: "https://balsamiq.com/",
-    color:"#AF040A"
+    color: "#AF040A",
   },
   {
     pic: "ripplematch.png",
     link: "https://ripplematch.com/",
-    color:"#3B8BEA"
+    color: "#3B8BEA",
   },
   {
     pic: "wolfram.png",
     link: "https://www.wolframalpha.com/",
-    color:"#FD755D"
+    color: "#FD755D",
   },
   {
     pic: "badabeanbadaboom.jpeg",
     link: "https://www.badabeansnacks.com/",
-    color:"#6C2310"
+    color: "#6C2310",
   },
   {
     pic: "echo3d.webp",
     link: "https://www.echo3d.com/",
-    color:"#002D64"
+    color: "#002D64",
   },
 ];
 
@@ -79,11 +79,7 @@ const Sponsors = () => {
             <Col lg={3} md={4} xs={6} key={index} className="p-4">
               <div>
                 <div className={`rounded-full hover:-translate-y-2`}>
-                  <a
-                    href={sponsor.link}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
+                  <a href={sponsor.link} target="_blank" rel="noreferrer">
                     <img
                       src={sponsor.pic}
                       className="border-about-right drop-shadow-blue border-4 rounded-full "

f12dc7705b5e2f7b6b94fc6f5cff69ba77dcd256
menthy-wu <wumengxuan0728@gmail.com>
2022-11-11 17:26:01 -0800

    sponsor link

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -3,13 +3,41 @@ import { FaCircle } from "react-icons/fa";
 import { Row, Col } from "react-bootstrap";
 
 const sponsors = [
-  "triad.jpeg",
-  "desmos.png",
-  "balsamiq.jpg",
-  "ripplematch.png",
-  "wolfram.png",
-  "badabeanbadaboom.jpeg",
-  "echo3d.webp",
+  {
+    pic: "triad.jpeg",
+    link: "https://www.triadmagnetics.com/",
+    color:"#F5BB2C"
+  },
+  {
+    pic: "desmos.png",
+    link: "https://www.desmos.com",
+    color:"#148B47"
+  },
+  {
+    pic: "balsamiq.jpg",
+    link: "https://balsamiq.com/",
+    color:"#AF040A"
+  },
+  {
+    pic: "ripplematch.png",
+    link: "https://ripplematch.com/",
+    color:"#3B8BEA"
+  },
+  {
+    pic: "wolfram.png",
+    link: "https://www.wolframalpha.com/",
+    color:"#FD755D"
+  },
+  {
+    pic: "badabeanbadaboom.jpeg",
+    link: "https://www.badabeansnacks.com/",
+    color:"#6C2310"
+  },
+  {
+    pic: "echo3d.webp",
+    link: "https://www.echo3d.com/",
+    color:"#002D64"
+  },
 ];
 
 const Sponsors = () => {
@@ -51,10 +79,16 @@ const Sponsors = () => {
             <Col lg={3} md={4} xs={6} key={index} className="p-4">
               <div>
                 <div className={`rounded-full hover:-translate-y-2`}>
-                  <img
-                    src={sponsor}
-                    className="border-team-green border-8 rounded-full "
-                  />
+                  <a
+                    href={sponsor.link}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={sponsor.pic}
+                      className="border-about-right drop-shadow-blue border-4 rounded-full "
+                    />
+                  </a>
                 </div>
               </div>
             </Col>

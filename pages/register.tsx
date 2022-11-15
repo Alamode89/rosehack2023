edf7f20f2423874ba642b642e4e49ec554c253d2
Divyank Shah <divyank.shah.2016@gmail.com>
2022-11-14 20:17:19 -0800

    add back button

--- pages/register.tsx
+++ pages/register.tsx
@@ -4,6 +4,8 @@ import { Row, Col } from "react-bootstrap";
 import Snackbar from "../components/Snackbar";
 import { schools } from "../components/data/schools";
 import Schools from "../components/Schools";
+import Link from "next/Link";
+import { FaChevronLeft } from "react-icons/fa";
 
 const grades = [
   "Less than Secondary / High School",
@@ -84,10 +86,17 @@ const Register = () => {
   return (
     <div className="flex justify-center items-center flex-col bg-gradient-to-b from-about-top to-about-bottom w-full">
       <div className="my-24 border-4 rounded-3xl border-white items-center flex flex-col w-10/12">
-        go back
-        <div className="font-pixel text-md md:text-xl lg:text-4xl p-0 text-white text-center w-1/2 m-4">
-          REGISTER
+        <div className="flex justify-center items-center  w-full relative">
+          <div className="absolute top-1/2 left-3 -translate-y-1/2">
+            <Link href="/">
+              <FaChevronLeft className="text-white text-5xl hover:!text-about-right hover:cursor-pointer" />
+            </Link>
+          </div>
+          <div className="font-pixel text-md md:text-xl lg:text-4xl p-0 text-white text-center w-full m-4">
+            REGISTER
+          </div>
         </div>
+
         {visible && (
           <Snackbar
             successful={true}

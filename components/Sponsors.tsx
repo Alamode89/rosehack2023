320bd0a7fbb5a486afaadd2f27f4ee0bf5d0c14c
Divyank Shah <divyank.shah.2016@gmail.com>
2022-11-15 19:29:17 -0800

    make data externally available

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -1,44 +1,7 @@
 import React from "react";
 import { FaCircle } from "react-icons/fa";
 import { Row, Col } from "react-bootstrap";
-
-const sponsors = [
-  {
-    pic: "triad.jpeg",
-    link: "https://www.triadmagnetics.com/",
-    color: "#F5BB2C",
-  },
-  {
-    pic: "desmos.png",
-    link: "https://www.desmos.com",
-    color: "#148B47",
-  },
-  {
-    pic: "balsamiq.jpg",
-    link: "https://balsamiq.com/",
-    color: "#AF040A",
-  },
-  {
-    pic: "ripplematch.png",
-    link: "https://ripplematch.com/",
-    color: "#3B8BEA",
-  },
-  {
-    pic: "wolfram.png",
-    link: "https://www.wolframalpha.com/",
-    color: "#FD755D",
-  },
-  {
-    pic: "badabeanbadaboom.jpeg",
-    link: "https://www.badabeansnacks.com/",
-    color: "#6C2310",
-  },
-  {
-    pic: "echo3d.webp",
-    link: "https://www.echo3d.com/",
-    color: "#002D64",
-  },
-];
+import { sponsors } from "./data/sponsors";
 
 const Sponsors = () => {
   return (

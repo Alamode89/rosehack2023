52ea75b55e5a323b3238eddef352f886f1d1ffd2
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-17 03:29:38 -0800

    formatting

--- pages/api/storeUser.ts
+++ pages/api/storeUser.ts
@@ -3,18 +3,16 @@ import { db } from "../../firebase";
 import { doc, setDoc } from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
 
-
 export default async function addStudent(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    req.body.team = uuidv4()
+    req.body.team = uuidv4();
     await setDoc(doc(db, "users", req.body.email), req.body);
     await setDoc(doc(db, "teams", req.body.team), {
       name: "Untitled Team",
-      members: [req.body.first + " " + req.body.first]
-
+      members: [req.body.first + " " + req.body.first],
     });
     res.status(200).json({});
   } catch (error) {

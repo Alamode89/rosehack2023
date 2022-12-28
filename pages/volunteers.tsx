5cf41ee17f483870db2f462e92854bd561fa2657
menthy-wu <wumengxuan0728@gmail.com>
2022-12-28 00:08:20 -0800

    styling volunteers

--- pages/volunteers.tsx
+++ pages/volunteers.tsx
@@ -1,37 +1,64 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import PinkHeart from "../public/pink_heart.webp";
+import Image from "next/image";
+import Machine3 from "../public/machine_3.webp";
 
 const volunteers = () => {
   return (
-    <div className="w-full flex justify-center items-center flex-col font-lexend bg-gradient-to-r from-header-left to-header-right text-white">
+    <div className="drop-shadow-lg w-full flex justify-center items-center flex-col font-lexend bg-gradient-to-r from-header-left to-header-right text-white">
       <div className="w-10/12 flex justify-center items-center flex-col">
-        <p className="text-4xl font-medium m-8 text-center">
-          Join Rosehack as a Volunteer or Mentor!
-        </p>
-        <p className="w-10/12 text-center text-lg">
-          Rose Hack is UC Riverside&apos;s women-center hackathon happening on
-          January 14-15, 2023. Thank you for showing interest in becoming a
-          volunteer or mentor, we truly appreciate all the help we can get. All
-          volunteers and mentors will be provided meals during breakfast, lunch
-          and dinner hours.
-        </p>
+        <div className=" flex justify-center items-center flex-row my-16">
+          <Image
+            src={PinkHeart}
+            alt=""
+            className="w-5 md:w-10 transform drop-shadow-pink"
+          />
+          <p className="text-xl md:text-3xl font-medium m-8 text-center drop-shadow-pink">
+            Join Rosehack as a Volunteer or Mentor!
+          </p>
+          <Image
+            src={PinkHeart}
+            alt=""
+            className="w-5 md:w-10 transform drop-shadow-pink"
+          />
+        </div>
+        <div className="w-10/12 flex flex-col md:flex-row justify-between items-center pb-12 my-6">
+          <Image
+            src={Machine3}
+            alt=""
+            className="w-1/4 transform drop-shadow-lg"
+          />
+          <p className="w-2/3 text-justify text-lg mt-4">
+            Rose Hack is UC Riverside&apos;s women-center hackathon happening on
+            January 14-15, 2023. Thank you for showing interest in becoming a
+            volunteer or mentor, we truly appreciate all the help we can get.
+            All volunteers and mentors will be provided meals during breakfast,
+            lunch and dinner hours.
+          </p>
+        </div>
+
         <Row className="w-full flex justify-center items-start">
           <Col
             xs={12}
             md={6}
             className="flex justify-center items-center flex-col"
           >
-            <p className="text-3xl font-medium">Volunteers</p>
+            <p className="text-3xl font-medium text-white drop-shadow-pink my-4">
+              Volunteers
+            </p>
             <a
               href="https://docs.google.com/forms/d/e/1FAIpQLSdaYzF8Zq8wki1SX9hmqg4glKBt6rt_SlB-TTr0hM1ybiRRUg/viewform"
               target="_blank"
               rel="noreferrer"
-              className="bg-blue-900 text-lg text-white no-underline px-4 py-2 rounded hover:scale-105"
+              className="bg-transparent hover:!bg-rose-pink border-2 border-white drop-shadow-pink text-lg text-white no-underline px-4 py-2 rounded-2xl hover:scale-105"
             >
               Become a Volunteer
             </a>
-            <p className="m-2 text-lg">Signups close January 12th</p>
+            <p className="m-2 text-base text-white drop-shadow-pink">
+              Signups close January 12th
+            </p>
             <ul className="list-disc leading-10 m-4 text-lg">
               <li>Assist with in-person hackathon operations</li>
               <li>Facilitate workshop and providing technological support</li>
@@ -43,16 +70,20 @@ const volunteers = () => {
             md={6}
             className="flex justify-start items-center flex-col"
           >
-            <p className="text-3xl font-medium">Mentors</p>
+            <p className="text-3xl font-medium text-white drop-shadow-blue my-4">
+              Mentors
+            </p>
             <a
               href="https://docs.google.com/forms/d/e/1FAIpQLSeHkULTC1m92z_XBEP5NbuLAYBMG8nkv2kVMQyytGdAkKjaIQ/viewform"
               target="_blank"
               rel="noreferrer"
-              className="bg-blue-900 text-lg text-white no-underline px-4 py-2 rounded hover:scale-105"
+              className="bg-transparent hover:!bg-rose-blue border-2 border-white drop-shadow-blue text-lg text-white no-underline px-4 py-2 rounded-2xl hover:scale-105"
             >
               Become a Mentor
             </a>
-            <p className="m-2 text-lg">Signups close January 12th</p>
+            <p className="m-2 text-base text-white drop-shadow-blue">
+              Signups close January 12th
+            </p>
             <ul className="list-disc leading-10 m-4 text-lg">
               <li>Inspire hackers to come up with creative solutions</li>
               <li>Share knowledge and experience with new hackers</li>
@@ -60,11 +91,11 @@ const volunteers = () => {
             </ul>
           </Col>
         </Row>
-        <p className="text-lg text-center">
+        <p className="text-lg text-center my-12">
           Have additional questions? Contact us at{" "}
           <a
             href="mailto:rosehackucr@gmail.com"
-            className="no-underline text-white"
+            className="no-underline text-rose-pink hover:text-rose-blue"
           >
             rosehackucr@gmail.com
           </a>

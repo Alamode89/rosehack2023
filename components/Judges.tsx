c345ba5c37880b0fef4e7fabf34c4ca97d474b88
Menthy <wumengxuan0728@gmail.com>
2023-01-14 00:46:41 -0800

    add 100vw

--- components/Judges.tsx
+++ components/Judges.tsx
@@ -83,7 +83,7 @@ const Judges = () => {
       </div>
 
       <div className="w-full justify-center mt-20">
-        <Image src={Pixels} alt="Pixels" />
+        <Image src={Pixels} alt="Pixels" className="w-[100vw]" />
       </div>
     </section>
   );
